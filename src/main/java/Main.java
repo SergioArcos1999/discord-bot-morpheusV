b3762247923d78067848c7a197a350d3880d4c5f
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2021-03-25 21:44:00 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String botToken = "";
+        builder.setToken(BOT_TOKEN);
+    }
+}
