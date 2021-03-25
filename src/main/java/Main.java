4c1142675574a15a3b0950bc0028d4c2a40a52b8
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2021-03-25 22:42:21 +0100

    Initial skeleton with a Listener & basic response interaction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,37 @@
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Main {
+import javax.security.auth.login.LoginException;
 
-    public static void main(String[] args) {
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String botToken = "";
-        builder.setToken(BOT_TOKEN);
+public class Main extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+        final String botToken = "ODI0NzQ0NzIzODEzMjM2Nzky.YFz1YQ.G-D9HVtyW6ztqwCecZnZkq78hP0";
+        JDABuilder JDA = JDABuilder.createDefault(botToken);
+        JDA.addEventListeners(new Main());
+        JDA.build();
+        System.out.println("jaja");
     }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String autor = event.getAuthor().getName();
+        String content = event.getMessage().getContentDisplay();
+        String rawContent = event.getMessage().getContentRaw();
+
+        System.out.println("We received a message from " +
+                autor +
+                ": " +
+                content);
+
+        if(rawContent.equals("!pussy")) {
+            event.getChannel().sendMessage("Pussy tu puta madre, zorra").queue();
+        }
+
+        if(event.getAuthor().isBot()) {
+            return;
+        }
+    }
+
 }
