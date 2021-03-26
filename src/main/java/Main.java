6a22d6e97577ff0741d91cd1492eaeb0c25bd3c3
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2021-03-26 20:53:34 +0100

    Setting activity status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import commons.utils.CustomFileReader;
 import listeners.ChatEventHandler;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
 
@@ -11,7 +14,10 @@ public class Main {
         CustomFileReader reader = new CustomFileReader();
         String botToken = reader.read("botToken");
 
-        JDABuilder builder = JDABuilder.createDefault(botToken);
-        builder.addEventListeners(new ChatEventHandler()).build();
+        JDA builder = JDABuilder.createDefault(botToken).build();
+        builder.addEventListener(new ChatEventHandler());
+        builder.getPresence().setStatus(OnlineStatus.ONLINE);
+        builder.getPresence().setActivity(Activity.watching("a tu puta madre"));
+
     }
 }
